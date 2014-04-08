d71b90ceeb7c4ee34594b811e594960aa726afe4
Shai Saar <avishay.saar@didata.com.au>
2014-04-08 11:22:31 +1000

    Added ignored backup* files

---
+++ ComputeClient/Compute.Contracts/Backup/BackupSchedulePolicies.cs
@@ -0,0 +1,31 @@
+﻿namespace DD.CBU.Compute.Api.Contracts.Backup
+{
+    using System;
+    using System.Xml.Serialization;
+
+    [System.CodeDom.Compiler.GeneratedCode("xsd", "4.0.30319.18020")]
+    [Serializable]
+    [System.Diagnostics.DebuggerStepThrough]
+    [System.ComponentModel.DesignerCategory("code")]
+    [XmlType(AnonymousType = true, Namespace = XmlNamespaceConstants.Backup)]
+    [XmlRoot(Namespace = XmlNamespaceConstants.Backup, IsNullable = false)]
+    public class BackupSchedulePolicies
+    {
+        [XmlElement(ElementName = "schedulePolicy")]
+        public BackupSchedulePolicy[] Items { get; set; }
+    }
+
+    [Serializable]
+    [System.Diagnostics.DebuggerStepThrough]
+    [System.ComponentModel.DesignerCategory("code")]
+    [XmlType(AnonymousType = true, Namespace = XmlNamespaceConstants.Backup)]
+    [XmlRoot(Namespace = XmlNamespaceConstants.Backup, IsNullable = false)]
+    public class BackupSchedulePolicy
+    {
+        [XmlAttribute]
+        public string name { get; set; }
+
+        [XmlAttribute]
+        public string description { get; set; }
+    }
+}
---
+++ ComputeClient/Compute.Contracts/Backup/BackupStoragePolicies.cs
@@ -0,0 +1,34 @@
+﻿namespace DD.CBU.Compute.Api.Contracts.Backup
+{
+    using System;
+    using System.Xml.Serialization;
+
+    [System.CodeDom.Compiler.GeneratedCode("xsd", "4.0.30319.18020")]
+    [Serializable]
+    [System.Diagnostics.DebuggerStepThrough]
+    [System.ComponentModel.DesignerCategory("code")]
+    [XmlType(AnonymousType = true, Namespace = XmlNamespaceConstants.Backup)]
+    [XmlRoot(Namespace = XmlNamespaceConstants.Backup, IsNullable = false)]
+    public class BackupStoragePolicies
+    {
+        [XmlElement(ElementName = "storagePolicy")]
+        public BackupStoragePolicy[] Items { get; set; }
+    }
+
+    [Serializable]
+    [System.Diagnostics.DebuggerStepThrough]
+    [System.ComponentModel.DesignerCategory("code")]
+    [XmlType(AnonymousType = true, Namespace = XmlNamespaceConstants.Backup)]
+    [XmlRoot(Namespace = XmlNamespaceConstants.Backup, IsNullable = false)]
+    public class BackupStoragePolicy
+    {
+        [XmlAttribute]
+        public string retentionPeriodInDays { get; set; }
+
+        [XmlAttribute]
+        public string name { get; set; }
+
+        [XmlAttribute]
+        public string secondaryLocation { get; set; }
+    }
+}
