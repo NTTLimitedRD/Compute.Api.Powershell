743608a374a72242f6326944d096714f21d13a96
Harry Sun <harry.sun@didata.com.au>
2014-06-18 14:38:00 +1000

    Refactor methods to extension methods

--- ComputeClient/Compute.Client/Interfaces/IComputeApiClient.cs
+++ ComputeClient/Compute.Client/Interfaces/IComputeApiClient.cs
@@ -165,44 +165,5 @@ namespace DD.CBU.Compute.Api.Client.Interfaces
         /// </summary>
         /// <returns></returns>
         Task<IEnumerable<ServerWithBackupType>> GetDeployedServers();
-
-        /// <summary>
-        /// Provision Net Terms Customer
-        /// </summary>
-        /// <param name="organizationId">
-        /// Organization Id
-        /// </param>
-        /// <param name="customerProvision">
-        /// The custom Provisioning.
-        /// </param>
-        /// <returns>
-        /// Provision Status
-        /// </returns>
-        Task<Status> Provision(Guid organizationId, CustomerProvision customerProvision);
-
-        /// <summary>
-        /// Provision Customer on a multi-geography data center
-        /// </summary>
-        /// <param name="organizationId">
-        /// The customer Id.
-        /// </param>
-        /// <param name="geographyId">
-        /// The geography Id.
-        /// </param>
-        /// <param name="customerPricingPlanKey">
-        /// The pricing Plan Key.
-        /// </param>
-        /// <returns>
-        /// Statuc
-        /// </returns>
-        Task<Status> ProvisionOnGeo(Guid organizationId, Guid geographyId, string customerPricingPlanKey);
-
-        /// <summary>
-        /// List pricing plans for a organization
-        /// </summary>
-        /// <param name="organizationId">Organization Id</param>
-        /// <param name="geoId">Geo Id</param>
-        /// <returns>Pricing Plans</returns>
-        Task<PricingPlans> ListPricingPlans(Guid organizationId, Guid geoId);
     }
 }
