312becdeb48357dfca0a086c725fb16300e6e038
Alex Bacchin <alexbacchin@hotmial.com>
2014-11-11 13:41:07 +1100

    Add-CaasServerDisk, Resize-CaasServerDisk, Remove-CaasServerDisk

---
+++ PowershellModule/CloudCompute.Powershell/AddCaasServerDiskCmdlet.cs
@@ -0,0 +1,73 @@
+﻿namespace DD.CBU.Compute.Powershell
+{
+    using System;
+    using System.Management.Automation;
+    using System.Linq;
+    using DD.CBU.Compute.Api.Client;
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// The new CaaS Virtual Machine cmdlet.
+    /// </summary>
+    [Cmdlet(VerbsCommon.Add, "CaasServerDisk")]
+ 
+
+    public class AddCaasServerDiskCmdlet : PsCmdletCaasBase
+    {
+        /// <summary>
+        /// The Server Details that will be used to deploy the VM
+        /// </summary>
+        [Parameter(Mandatory = true, ValueFromPipeline = true, HelpMessage = "The server details created by New-CaasServerDetails")]
+        public ServerWithBackupType Server { get; set; }
+
+
+        
+        [Parameter(Mandatory = true, HelpMessage = "The new disk size")]
+        public int SizeInGB { get; set; }
+
+
+        [Parameter(Mandatory = false, HelpMessage = "The speedId of the new disk. The available speed Id can be retrieved using (Get-CaasDataCentre).hypervisor.diskSpeed")]
+        public string SpeedId { get; set; }
+
+        /// <summary>
+        /// The process record method.
+        /// </summary>
+        protected override void ProcessRecord()
+        {
+
+            base.ProcessRecord();
+            try
+            {
+                var status = CaaS.ApiClient.AddServerDisk(Server.id, SizeInGB.ToString(),SpeedId).Result;
+                if (status != null)
+                    WriteDebug(
+                        string.Format(
+                            "{0} resulted in {1} ({2}): {3}",
+                            status.operation,
+                            status.result,
+                            status.resultCode,
+                            status.resultDetail));
+                
+            }
+            catch (AggregateException ae)
+            {
+                ae.Handle(
+                    e =>
+                    {
+                        if (e is ComputeApiException)
+                        {
+                            WriteError(new ErrorRecord(e, "-2", ErrorCategory.InvalidOperation, CaaS));
+                        }
+                        else //if (e is HttpRequestException)
+                        {
+                            ThrowTerminatingError(new ErrorRecord(e, "-1", ErrorCategory.ConnectionError, CaaS));
+                        }
+                        return true;
+                    });
+            }
+
+        }
+
+        
+    }
+}
---
+++ PowershellModule/CloudCompute.Powershell/ResizeCaasServerDiskCmdlet.cs
@@ -0,0 +1,85 @@
+﻿namespace DD.CBU.Compute.Powershell
+{
+    using System;
+    using System.Linq;
+    using System.Management.Automation;
+
+    using DD.CBU.Compute.Api.Client;
+
+    /// <summary>
+    /// The set server state cmdlet.
+    /// </summary>
+    [Cmdlet(VerbsCommon.Resize, "CaasServerDisk")]
+    public class ResizeCaasServerDiskCmdlet : PsCmdletCaasBase
+    {
+        
+        [Parameter(Mandatory = true, ValueFromPipeline = true, HelpMessage = "The server to action on")]
+        public ServerWithBackupType Server { get; set; }
+
+         [Parameter(Mandatory = true, HelpMessage = "SCSI Id of the disk to be resized")]
+        public int ScsiId { get; set; }
+
+         [Parameter(Mandatory = true, HelpMessage = "New disk size in GB")]
+         public int NewSizeInGB { get; set; }
+
+
+        
+
+        /// <summary>
+        /// The process record method.
+        /// </summary>
+        protected override void ProcessRecord()
+        {
+            base.ProcessRecord();
+
+            SetServerTask();
+        }
+
+        /// <summary>
+        /// Edit the server disk details
+        /// </summary>
+        private void SetServerTask()
+        {
+            try
+            {
+                Status status = null;
+
+                var disk = Server.disk.Where(d => d.scsiId == ScsiId);
+                if (disk.Any())
+                {
+
+                    status = CaaS.ApiClient.ChangeServerDiskSize(Server.id, disk.ElementAt(0).id, NewSizeInGB.ToString()).Result;
+
+                }
+                else
+                    WriteError(new ErrorRecord(new PSArgumentException("The scsi id does not exits"), "-1", ErrorCategory.InvalidArgument, null));
+
+                    if (status != null)
+                    WriteDebug(
+                        string.Format(
+                            "{0} resulted in {1} ({2}): {3}",
+                            status.operation,
+                            status.result,
+                            status.resultCode,
+                            status.resultDetail));
+
+            }
+            catch (AggregateException ae)
+            {
+                ae.Handle(
+                    e =>
+                        {
+                            if (e is ComputeApiException)
+                            {
+                                WriteError(new ErrorRecord(e, "-2", ErrorCategory.InvalidOperation, CaaS));
+                            }
+                            else //if (e is HttpRequestException)
+                            {
+                                ThrowTerminatingError(new ErrorRecord(e, "-1", ErrorCategory.ConnectionError, CaaS));
+                            }
+                            return true;
+                        });
+            }
+        }
+    }
+}
