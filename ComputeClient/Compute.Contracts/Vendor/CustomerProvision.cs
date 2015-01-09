1f37cda00d5dfb495f60d23b3536d94388a8663f
alexbacchin <alexbacchin@hotmail.com>
2015-01-09 15:57:41 +1100

    updated version to 1.3

---
+++ ComputeClient/Compute.Contracts/Vendor/CustomerProvision.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Xml.Serialization;
+
+namespace DD.CBU.Compute.Api.Contracts.Vendor
+{
+    /// <summary>
+    /// A new customer to do provisioning
+    /// </summary>
+    [GeneratedCode("xsd", "4.0.30319.1")]
+    [Serializable]
+    [DebuggerStepThrough]
+    [DesignerCategory("code")]
+    [XmlType(AnonymousType = true, Namespace = XmlNamespaceConstants.Organization)]
+    [XmlRoot(Namespace = XmlNamespaceConstants.Organization, IsNullable = false)]
+    public class CustomerProvision
+    {
+        /// <remarks/>
+        public string companyName;
+
+        /// <remarks/>
+        public int trustLevel;
+
+        /// <remarks/>
+        [XmlElement("billingDetails")]
+        public BillingDetails billingDetails;
+
+        /// <remarks/>
+        [XmlElement("primaryAdministrator")]
+        public PrimaryAdministrator primaryAdministrator;
+
+        /// <remarks/>
+        [XmlElement("contact")]
+        public Contact contact;
+    }
+}
