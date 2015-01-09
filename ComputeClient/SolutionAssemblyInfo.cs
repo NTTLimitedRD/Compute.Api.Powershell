0e3a7a84c6cf2d38b6e21772a6cdadd47f28ece0
alexbacchin <alexbacchin@hotmail.com>
2015-01-09 15:57:41 +1100

    updated version to 1.3

--- ComputeClient/SolutionAssemblyInfo.cs
+++ ComputeClient/SolutionAssemblyInfo.cs
@@ -2,8 +2,8 @@
 
 [assembly: AssemblyCompany("Dimension Data")]
 [assembly: AssemblyProduct("Compute as a Service (CaaS) API client.")]
-[assembly: AssemblyCopyright("Copyright © Dimension Data 2014")]
+[assembly: AssemblyCopyright("Copyright © Dimension Data 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.2.0.0")]
+[assembly: AssemblyFileVersion("1.3.0.0")]
