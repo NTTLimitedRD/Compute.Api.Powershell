4edc844844474ba1b9f0fddf78170dc6c9882930
alexbacchin <alexbacchin@hotmail.com>
2015-01-14 14:20:11 +1100

    added PS Examples

---
+++ PowerShell Examples/Connection.ps1
@@ -0,0 +1,27 @@
+﻿
+$connectionDev=New-CaasConnection -Name "AustraliaConnection" -ApiCredentials (Get-Credential -Message "Dev Connection") -Vendor DimensionData -Region Australia_AU 
+
+$connectionDev2=New-CaasConnection -Name "APACConnection" -ApiCredentials (Get-Credential -Message "Dev2 Connection") -Vendor DimensionData -Region AsiaPacific_AP
+
+
+Write-Host "Get Deployed Servers from connection AustraliaConnection"
+
+#list deployed servers on the active connection (AustraliaConnection)
+Get-CaasDeployedServer
+
+#set the active connection to APACConnection
+Set-CaasActiveConnection -Name "APACConnection"
+
+
+
+Write-Host "Get Deployed Servers from connection APACConnection"
+#list deployed servers on the active connection (APACConnection)
+Get-CaasDeployedServer
+
+
+#use the connection parameter to execute the cmdlet agains the AustraliaConnection 
+Get-CaasDeployedServer -Connection $connectionDev
+
+
+#list all connections stored on the PS session 
+Get-CaasConnection
---
+++ PowerShell Examples/CreateVMBackground.ps1
@@ -0,0 +1,37 @@
+﻿#Requirements
+
+#import Caas Module
+Import-Module CaaS
+
+#capture the Caas credentials and create a new Caas conneciton
+$login = Get-Credential
+
+$scriptblock = { `
+
+$servername = "Server1"
+$networkname = "Network1"
+$osimagename = "Win2012 DC 64-bit 2 CPU"
+$administratorPassword= "password123"
+
+
+New-CaasConnection -ApiCredentials $args[0] -Vendor DimensionData -Region Australia_AU
+
+
+#Get the network with a specific name
+$network=Get-CaasNetworks -Name $networkname
+
+#Get a OS image with a specific name 
+$os=Get-CaasOsImages -Network $network -Name $osimagename
+
+#create a new server details configuration
+$serverdetails = New-CaasServerDetails -Name $servername  -AdminPassword $administratorPassword -IsStarted $false -OsServerImage $os -Network $network
+
+# create new server, change memory to 8GB and 4 vCPU, start the server
+
+ New-CaasServer -ServerDetails $serverdetails -PassThru | Out-CaasWaitForOperation | Set-CaasServer -CPUCount 4 -MemoryInMB 8192 -PassThru | Out-CaasWaitForOperation | Set-CaasServerState -Action PowerOn 
+
+ }
+
+ Start-Job -Name "CreateServer" -ScriptBlock $scriptblock -ArgumentList $login
+
+
