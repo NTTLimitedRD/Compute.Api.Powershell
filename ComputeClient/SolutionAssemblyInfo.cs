79fff4fc46b1fe7c39a0e4f668f4ce6dcc8edd43
alexbacchin <alexbacchin@hotmail.com>
2015-01-16 09:08:11 +1100

    update build to 1.3.0.2

--- ComputeClient/SolutionAssemblyInfo.cs
+++ ComputeClient/SolutionAssemblyInfo.cs
@@ -6,4 +6,4 @@
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.3.0.1")]
+[assembly: AssemblyFileVersion("1.3.0.2")]
