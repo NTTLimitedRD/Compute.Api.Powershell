dc796269aa6251f8f451c75a9751486b51b58366
alexbacchin <alexbacchin@hotmail.com>
2015-01-29 08:42:37 +1100

    Update solution version to 1.3.1

--- ComputeClient/SolutionAssemblyInfo.cs
+++ ComputeClient/SolutionAssemblyInfo.cs
@@ -6,4 +6,4 @@
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.3.0.2")]
+[assembly: AssemblyFileVersion("1.3.1.0")]
