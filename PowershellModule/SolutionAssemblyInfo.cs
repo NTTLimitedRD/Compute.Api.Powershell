27e7b9b2e23baa9aae3217c66521998d57166d8d
alexbacchin <alexbacchin@hotmail.com>
2015-04-27 13:31:57 +1000

    updated version to 2.0.0.0

--- PowershellModule/SolutionAssemblyInfo.cs
+++ PowershellModule/SolutionAssemblyInfo.cs
@@ -15,4 +15,4 @@ using System.Reflection;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.3.2.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
