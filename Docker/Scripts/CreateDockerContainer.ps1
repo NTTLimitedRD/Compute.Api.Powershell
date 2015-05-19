52ffcce695899498a32da037e5c16e6d78430bd7
Manish Kumar <manishkp@live.com>
2015-05-19 15:19:58 +1000

    Adding Docker run command

---
+++ Docker/Scripts/CreateDockerContainer.ps1
@@ -0,0 +1,11 @@
+﻿#import Caas Module
+Import-Module "C:\Sources\DD\DimensionData.ComputeClient\PowershellModule\CloudCompute.Powershell\bin\Debug\CaaS.psd1"
+#Import-Module CaaS
+Set-Location $PSScriptRoot
+Import-Module (Join-Path $PSScriptRoot "Docker.psm1") -DisableNameChecking
+
+#capture the Caas credentials and create a new Caas conneciton
+$login = Get-Credential -Message "Enter Caas api credentials"
+New-CaasConnection -ApiCredentials $login -ApiBaseUri https://api-au.dimensiondata.com/oec/0.9/
+
+New-CaasDockerContainer -Name "DockerHost19" -NetworkName "Innovation Lab" -RootPassword "password123"
