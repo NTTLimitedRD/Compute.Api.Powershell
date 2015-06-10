e89095be559cf18f608464bd79fd5a8eb504505e
Anthony Shaw <anthony.p.shaw@gmail.com>
2015-06-10 16:11:33 +1000

    Changed test network name

--- Docker/Scripts/DockerTest.ps1
+++ Docker/Scripts/DockerTest.ps1
@@ -5,7 +5,7 @@ Function Test-CaasDockerContainer {
 	$login = Get-Credential -Message "Enter CaaS API credentials"
 	New-CaasConnection -ApiCredentials $login -Vendor DimensionData -Region Australia_AU
 
-	New-CaasDockerHost -Name "Test-DockerHost" -NetworkName "ANT-WEB" -RootPassword "unicorndatapass"
+	New-CaasDockerHost -Name "Test-DockerHost" -NetworkName "TEST-WEB" -RootPassword "unicorndatapass"
 
-	Invoke-CaasDockerContainer -Name "tomcat7" -Parameters "-p 8080:8080 -d" -NetworkName "ANT-WEB" -RootPassword "unicorndatapass"
+	Invoke-CaasDockerContainer -Name "tomcat:8.0" -Parameters "-p 8080:8080 -d" -NetworkName "TEST-WEB" -RootPassword "unicorndatapass"
 }
