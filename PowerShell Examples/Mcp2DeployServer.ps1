475cdc05b61a6c178f38b0d93ba8614da3e15f87
Anthony Shaw <anthony.p.shaw@gmail.com>
2015-07-20 15:27:03 -0400

    Updated MCP 2 script

--- PowerShell Examples/Mcp2DeployServer.ps1
+++ PowerShell Examples/Mcp2DeployServer.ps1
@@ -1,15 +1,15 @@
 ﻿$networkDomainName = "Test API network"
-$location = "NASH_PCS01_N2_VMWARE_1"
+$location = "NA9"
 $osImageName = "RedHat 6 64-bit 2 CPU"
 $administratorPassword= "password123"
 $vlanName = "testV1"
 
 #import Caas Module
-#Import-Module CaaS
-Import-Module "C:\Sources\DD\DimensionData.ComputeClient\PowershellModule\CloudCompute.Powershell\bin\Debug\CaaS.psd1"
+Import-Module CaaS
+
 #capture the Caas credentials and create a new Caas conneciton
 $login = Get-Credential
-New-CaasConnection -ApiCredentials $login -ApiDomainName apinashpcs01.opsourcecloud.net
+New-CaasConnection -ApiCredentials $login -Region NorthAmerica_NA
 
 #Get the network with a specific name
 $networkDomain = Get-CaasNetworkDomain -NetworkDomainName $networkDomainName
@@ -18,6 +18,6 @@ $networkDomain = Get-CaasNetworkDomain -NetworkDomainName $networkDomainName
 $os=Get-CaasOsImages -Location $location -Name $osImageName
 
 #create a new server details configuration
-$vlan = get-CaasVlan -Name $vlanName -NetworkDomain $networkDomain
+$vlan = Get-CaasVlan -Name $vlanName -NetworkDomain $networkDomain
 
 new-CaasServerOnNetworkDomain -Name "MyTestServer" -Description "Server Created using PS" -NetworkDomain $networkDomain -ServerImage $os -IsStarted $True -AdminPassword $administratorPassword -PrimaryNetwork $vlan 
