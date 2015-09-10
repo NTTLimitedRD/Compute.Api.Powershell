b547217c9b4fe7b4c366aae26210ba9e042e606b
Manish Kumar <manishkp@gmail.com>
2015-09-10 16:05:53 +1000

    creating a realease 3.0.3

--- PowershellModule/SolutionAssemblyInfo.cs
+++ PowershellModule/SolutionAssemblyInfo.cs
@@ -14,5 +14,5 @@ using System.Reflection;
 [assembly: AssemblyCopyright("Copyright © Dimension Data 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("3.0.2.0")]
+[assembly: AssemblyVersion("3.0.3.0")]
+[assembly: AssemblyFileVersion("3.0.3.0")]
