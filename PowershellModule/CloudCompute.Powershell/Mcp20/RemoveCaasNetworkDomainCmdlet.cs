af401e236b64fb18c8b5f822f660badf89b73d2f
Manish Kumar <manishkp@gmail.com>
2015-11-25 12:49:42 +1100

    Adding Remove-vlan and remove-networkdomain commands

---
+++ PowershellModule/CloudCompute.Powershell/Mcp20/RemoveCaasNetworkDomainCmdlet.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Management.Automation;
+using DD.CBU.Compute.Api.Client;
+using DD.CBU.Compute.Api.Contracts.Network20;
+
+namespace DD.CBU.Compute.Powershell.Mcp20
+{
+    /// <summary>
+    ///     The Remove Network Domain CmdLet
+    /// </summary>
+    [Cmdlet(VerbsCommon.Remove, "CaasNetworkDomain")]
+    [OutputType(typeof(ResponseType))]
+    public class RemoveCaasNetworkDomainCmdlet : PSCmdletCaasWithConnectionBase
+    {
+        /// <summary>
+        ///     Gets or sets the Network Domain.
+        /// </summary>
+        [Parameter(Mandatory = true, ValueFromPipeline = true, HelpMessage = "The Network Domain")]
+        public NetworkDomainType NetworkDomain { get; set; }
+
+        /// <summary>
+        ///     The process record method.
+        /// </summary>
+        protected override void ProcessRecord()
+        {
+            ResponseType response = null;
+            base.ProcessRecord();
+            try
+            {    
+                response = Connection.ApiClient.Networking.NetworkDomain.DeleteNetworkDomain(Guid.Parse(NetworkDomain.id)).Result;
+            }
+            catch (AggregateException ae)
+            {
+                ae.Handle(
+                    e =>
+                    {
+                        if (e is ComputeApiException)
+                        {
+                            WriteError(new ErrorRecord(e, "-2", ErrorCategory.InvalidOperation, Connection));
+                        }
+                        else
+                        {
+                            // if (e is HttpRequestException)
+                            ThrowTerminatingError(new ErrorRecord(e, "-1", ErrorCategory.ConnectionError, Connection));
+                        }
+
+                        return true;
+                    });
+            }
+
+            WriteObject(response);
+        }
+    }
+}
---
+++ PowershellModule/CloudCompute.Powershell/Mcp20/RemoveCaasVlanCmdlet.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Management.Automation;
+using DD.CBU.Compute.Api.Client;
+using DD.CBU.Compute.Api.Contracts.Network20;
+
+namespace DD.CBU.Compute.Powershell.Mcp20
+{
+    /// <summary>
+    ///     The Remove Vlan CmdLet
+    /// </summary>
+    [Cmdlet(VerbsCommon.Remove, "CaasVlan")]
+    [OutputType(typeof(ResponseType))]
+    public class RemoveCaasVlanCmdlet : PSCmdletCaasWithConnectionBase
+    {
+        /// <summary>
+        ///     Gets or sets the Vlan.
+        /// </summary>
+        [Parameter(Mandatory = true, ValueFromPipeline = true, HelpMessage = "The Vlan")]
+        public VlanType Vlan { get; set; }
+
+        /// <summary>
+        ///     The process record method.
+        /// </summary>
+        protected override void ProcessRecord()
+        {
+            ResponseType response = null;
+            base.ProcessRecord();
+            try
+            {    
+                response = Connection.ApiClient.Networking.Vlan.DeleteVlan(Guid.Parse(Vlan.id)).Result;
+            }
+            catch (AggregateException ae)
+            {
+                ae.Handle(
+                    e =>
+                    {
+                        if (e is ComputeApiException)
+                        {
+                            WriteError(new ErrorRecord(e, "-2", ErrorCategory.InvalidOperation, Connection));
+                        }
+                        else
+                        {
+                            // if (e is HttpRequestException)
+                            ThrowTerminatingError(new ErrorRecord(e, "-1", ErrorCategory.ConnectionError, Connection));
+                        }
+
+                        return true;
+                    });
+            }
+
+            WriteObject(response);
+        }
+    }
+}
