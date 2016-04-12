47dbfa886a8815cae75ec49876129a6ef4b12e70
Manish Kumar <manish.kumar@itaas.dimensiondata.com>
2016-04-12 17:18:54 +1000

    Adding update version ps1

---
+++ ComputeClient/UpdateAssemblyInfo.ps1
@@ -0,0 +1,30 @@
+﻿Param(
+	[ValidateNotNullOrEmpty()]
+	[Parameter(Mandatory = $true)]
+	[String] $ReleaseVersion
+)
+
+Write-Host "Updating solution versions to $ReleaseVersion";
+
+Function UpdateAssemblyInfoWithBuildNumber([string] $solutionAssemblyInfoFile, [string] $version)
+{
+    $solutionAssemblyInfo = Get-Content $solutionAssemblyInfoFile
+    $updatedVersionInfo = $solutionAssemblyInfo -replace "Version\(`"(\d+)\.(\d+)\.(\d+)\.\d+`"\)\]`$", "Version(`"$version`")]"
+
+    $updatedVersionInfo | Out-File $solutionAssemblyInfoFile
+}
+
+Function UpdateNuSpecWithBuildNumber([string] $nuSpecFile, [string] $version)
+{
+    Write-Host "Updating nuget versions for $nuSpecFile to $majorVersion.$minorVersion.$buildId.0";
+    $nuSpec = Get-Content $nuSpecFile
+    $nuSpec = $nuSpec -replace "\<version\>(\d+)\.(\d+)\.(\d+).(\d+)\<\/version\>", "<version>$version</version>"
+    $nuSpec = $nuSpec -replace "[\$]version\$", "$version"
+
+    Set-Content $nuSpecFile -Value $nuSpec
+}
+
+$currentDir = (Get-Location).Path
+UpdateAssemblyInfoWithBuildNumber(Join-Path $currentDir "SolutionAssemblyInfo.cs")
+UpdateNuSpecWithBuildNumber(Join-Path $currentDir "Compute.Client\Compute.Client.nuspec")
+
