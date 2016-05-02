a7a2686e76de11cba3846f2a4872d843c3eabbbf
Hamed Soleymani <hamed.soleymani@itaas.dimsnsiondata.com>
2016-05-02 15:04:17 +1000

    Apply the changes in API 2.2

--- PowershellModule/CloudCompute.Powershell/AddCaasServerDiskCmdlet.cs
+++ PowershellModule/CloudCompute.Powershell/AddCaasServerDiskCmdlet.cs
@@ -12,82 +12,85 @@ using System.Globalization;
 using System.Management.Automation;
 using DD.CBU.Compute.Api.Client;
 using DD.CBU.Compute.Api.Contracts.General;
-using DD.CBU.Compute.Api.Contracts.Server;
 
 namespace DD.CBU.Compute.Powershell
 {
-	/// <summary>
-	///     The new CaaS Virtual Machine cmdlet.
-	/// </summary>
-	[Cmdlet(VerbsCommon.Add, "CaasServerDisk")]
-	public class AddCaasServerDiskCmdlet : PsCmdletCaasServerBase
-	{
-		/// <summary>
-		///     Gets or sets the size in gb.
-		/// </summary>
-		[Parameter(Mandatory = true, HelpMessage = "The new disk size")]
-		public int SizeInGB { get; set; }
+    using DD.CBU.Compute.Api.Contracts.Network20;
 
+    using DiskSpeedType = DD.CBU.Compute.Api.Contracts.Server.DiskSpeedType;
 
-		/// <summary>
-		///     Gets or sets the speed id.
-		/// </summary>
-		[Parameter(Mandatory = false, ParameterSetName = "SpeedId", 
-			HelpMessage =
-				"The speedId of the new disk. The available speed Id can be retrieved using (Get-CaasDataCentre).hypervisor.diskSpeed"
-			)]
-		public string SpeedId { get; set; }
+    /// <summary>
+    ///     The new CAAS Virtual Machine CMDLET.
+    /// </summary>
+    [Cmdlet(VerbsCommon.Add, "CaasServerDisk")]
+    public class AddCaasServerDiskCmdlet : PsCmdletCaasServerBase
+    {
+        /// <summary>
+        ///     Gets or sets the size in gb.
+        /// </summary>
+        [Parameter(Mandatory = true, HelpMessage = "The new disk size")]
+        public int SizeInGB { get; set; }
 
+        /// <summary>
+        ///     Gets or sets the speed id.
+        /// </summary>
+        [Parameter(Mandatory = false, ParameterSetName = "SpeedId", HelpMessage = "The speedId of the new disk. The available speed Id can be retrieved using (Get-CaasDataCentre).hypervisor.diskSpeed")]
+        public string SpeedId { get; set; }
 
-		/// <summary>
-		///     Gets or sets the speed.
-		/// </summary>
-		[Parameter(Mandatory = false, ParameterSetName = "DiskSpeedType", HelpMessage = "The disk speed to be created")]
-		public DiskSpeedType Speed { get; set; }
+        /// <summary>
+        ///     Gets or sets the speed.
+        /// </summary>
+        [Parameter(Mandatory = false, ParameterSetName = "DiskSpeedType", HelpMessage = "The disk speed to be created")]
+        public DiskSpeedType Speed { get; set; }
 
+        [Parameter(Mandatory = false, ParameterSetName = "ScsiId", HelpMessage = "The Scsi Id")]
+        public int? ScsiId { get; set; }
 
-		/// <summary>
-		///     The process record method.
-		/// </summary>
-		protected override void ProcessRecord()
-		{
-			try
-			{
-				if (ParameterSetName.Equals("DiskSpeedType"))
-					SpeedId = Speed.ToString();
-				Status status =
-					Connection.ApiClient.ServerManagementLegacy.Server.AddServerDisk(Server.id, 
-						SizeInGB.ToString(CultureInfo.InvariantCulture), SpeedId).Result;
-				if (status != null)
-					WriteDebug(
-						string.Format(
-							"{0} resulted in {1} ({2}): {3}", 
-							status.operation, 
-							status.result, 
-							status.resultCode, 
-							status.resultDetail));
+        /// <summary>
+        ///     The process record method.
+        /// </summary>
+        protected override void ProcessRecord()
+        {
+            base.ProcessRecord();
+            ResponseType response = null;
+            try
+            {
+                if (ParameterSetName.Equals("DiskSpeedType"))
+                {
+                    SpeedId = Speed.ToString();
+                }
 
+                response =
+                    Connection.ApiClient.ServerManagement.Server.AddDisk(
+                        new AddDiskType
+                        {
+                            id = Server.id,
+                            sizeGb = SizeInGB,
+                            speed = Speed.ToString(),
+                            scsiId = ScsiId ?? 0,
+                            scsiIdSpecified = ScsiId.HasValue
+                        }).Result;
+            }
+            catch (AggregateException ae)
+            {
+                ae.Handle(
+                    e =>
+                    {
+                        if (e is ComputeApiException)
+                        {
+                            WriteError(new ErrorRecord(e, "-2", ErrorCategory.InvalidOperation, Connection));
+                        }
+                        else
+                        {
+                            // if (e is HttpRequestException)
+                            ThrowTerminatingError(new ErrorRecord(e, "-1", ErrorCategory.ConnectionError, Connection));
+                        }
 
-				base.ProcessRecord();
-			}
-			catch (AggregateException ae)
-			{
-				ae.Handle(
-					e =>
-					{
-						if (e is ComputeApiException)
-						{
-							WriteError(new ErrorRecord(e, "-2", ErrorCategory.InvalidOperation, Connection));
-						}
-						else
-						{
-// if (e is HttpRequestException)
-							ThrowTerminatingError(new ErrorRecord(e, "-1", ErrorCategory.ConnectionError, Connection));
-						}
+                        return true;
+                    });
+            }
 
-						return true;
-					});
-			}
-		}
-	}
+            WriteObject(response);
+        }
+    }
 }
--- PowershellModule/CloudCompute.Powershell/RemoveCaasServerDiskCmdlet.cs
+++ PowershellModule/CloudCompute.Powershell/RemoveCaasServerDiskCmdlet.cs
@@ -18,75 +18,67 @@ using DD.CBU.Compute.Api.Contracts.Network20;
 
 namespace DD.CBU.Compute.Powershell
 {
-	/// <summary>
-	///     The set server state cmdlet.
-	/// </summary>
-	[Cmdlet(VerbsCommon.Remove, "CaasServerDisk", SupportsShouldProcess = true)]
-	public class RemoveCaasServerDiskCmdlet : PsCmdletCaasServerBase
-	{
-		/// <summary>
-		///     Gets or sets the scsi id.
-		/// </summary>
-		[Parameter(Mandatory = true, HelpMessage = "SCSI Id of the disk to be resized")]
-		public int ScsiId { get; set; }
+    [Cmdlet(VerbsCommon.Remove, "CaasServerDisk", SupportsShouldProcess = true)]
+    public class RemoveCaasServerDiskCmdlet : PsCmdletCaasServerBase
+    {
+        [Parameter(Mandatory = true, ParameterSetName = "With_SCSIId", HelpMessage = "SCSI Id of the disk to be resized")]
+        public int ScsiId { get; set; }
 
+        [Parameter(Mandatory = true, ParameterSetName = "With_DiskId", HelpMessage = "The id of Disk")]
+        public string Id { get; set; }
 
-		/// <summary>
-		///     The process record method.
-		/// </summary>
-		protected override void ProcessRecord()
-		{
-			if (!ShouldProcess(string.Concat("Disk Id", ScsiId.ToString(CultureInfo.InvariantCulture)))) return;
-			SetServerTask();
-			base.ProcessRecord();
-		}
+        /// <summary>
+        ///     The process record method.
+        /// </summary>
+        protected override void ProcessRecord()
+        {
+            base.ProcessRecord();
+            try
+            {
+                switch (ParameterSetName)
+                {
+                    case "With_DiskId":
+                        break;
+                    default:
+                        if (!ShouldProcess(string.Concat("Disk SCACI Id", ScsiId.ToString(CultureInfo.InvariantCulture))))
+                        {
+                            return;
+                        }
 
-		/// <summary>
-		///     Edit the server disk details
-		/// </summary>
-		private void SetServerTask()
-		{
-			try
-			{
-				Status status = null;
+                        if (Server.disk.Any(d => d.scsiId == ScsiId))
+                        {
+                            Id = Server.disk.First(d => d.scsiId == ScsiId).id;
+                        }
 
-				IEnumerable<ServerTypeDisk> disk = Server.disk.Where(d => d.scsiId == ScsiId);
-				if (disk.Any())
-				{
-					status =
-						Connection.ApiClient.ServerManagementLegacy.Server.RemoveServerDisk(Server.id, disk.ElementAt(0).id).Result;
-				}
-				else
-					WriteError(new ErrorRecord(new PSArgumentException("The scsi id does not exits"), "-1", 
-						ErrorCategory.InvalidArgument, null));
+                        break;
+                }
 
-				if (status != null)
-					WriteDebug(
-						string.Format(
-							"{0} resulted in {1} ({2}): {3}", 
-							status.operation, 
-							status.result, 
-							status.resultCode, 
-							status.resultDetail));
-			}
-			catch (AggregateException ae)
-			{
-				ae.Handle(
-					e =>
-					{
-						if (e is ComputeApiException)
-						{
-							WriteError(new ErrorRecord(e, "-2", ErrorCategory.InvalidOperation, Connection));
-						}
-						else
-						{
-// if (e is HttpRequestException)
-							ThrowTerminatingError(new ErrorRecord(e, "-1", ErrorCategory.ConnectionError, Connection));
-						}
+                if (!ShouldProcess(string.Concat("Disk Id", Id.ToString(CultureInfo.InvariantCulture))))
+                {
+                    return;
+                }
 
-						return true;
-					});
-			}
-		}
-	}
+                WriteObject(Connection.ApiClient.ServerManagement.Server.RemoveDisk(new RemoveDiskType { id = Id }).Result);
+            }
+            catch (AggregateException ae)
+            {
+                ae.Handle(
+                    e =>
+                        {
+                            if (e is ComputeApiException)
+                            {
+                                WriteError(new ErrorRecord(e, "-2", ErrorCategory.InvalidOperation, Connection));
+                            }
+                            else
+                            {
+                                // if (e is HttpRequestException)
+                                ThrowTerminatingError(
+                                    new ErrorRecord(e, "-1", ErrorCategory.ConnectionError, Connection));
+                            }
+
+                            return true;
+                        });
+            }
+        }
+    }
 }
