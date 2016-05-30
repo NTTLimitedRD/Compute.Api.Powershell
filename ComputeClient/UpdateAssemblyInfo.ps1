e07e668a981c4b3e188c620968796d2e963eb36b
Manish Kumar <manish.kumar@itaas.dimensiondata.com>
2016-05-30 16:58:51 +1000

    Moving back to non sem ver

--- ComputeClient/UpdateAssemblyInfo.ps1
+++ ComputeClient/UpdateAssemblyInfo.ps1
@@ -14,7 +14,11 @@ Function Get-BuildVersion()
         # ProductVersion is Major.Minor eg 1.2
         [Parameter(Mandatory=$True)]
         [ValidateNotNullOrEmpty()]
-        [string] $ProductVersion
+        [string] $ProductVersion,
+            
+        [ValidateNotNullOrEmpty()]
+	    [Parameter(Mandatory = $true)]
+	    [String] $ReleaseTag
     )
     
     # Expecting BUILD_BUILDURI be something like vstfs:///Build/Build/35
@@ -30,7 +34,7 @@ Function Get-BuildVersion()
     Write-Host "BuildUri : $buildUri";
     $buildId = Split-Path -Leaf $buildUri.LocalPath
     Write-Host "buildId : $($buildUri.LocalPath)";
-    return @{ AssemblyVersion = "$($parsedVersion.Major).$($parsedVersion.Minor).$($buildId).0" ; PackageVersion = "$($parsedVersion.Major).$($parsedVersion.Minor).$($buildId)"}
+    return @{ AssemblyVersion = "$($parsedVersion.Major).$($parsedVersion.Minor).$($buildId).0" ; PackageVersion = "$($parsedVersion.Major).$($parsedVersion.Minor).$($buildId).0-$ReleaseTag"}
 }
 import-module .\UpdateAssemblyInfo.psm1
 $buildVersion = Get-BuildVersion -ProductVersion $ProductVersion
@@ -38,5 +42,5 @@ Write-Host "Updating solution versions to $($buildVersion.AssemblyVersion) and p
 
 $currentDir = (Get-Location).Path
 Update-AssemblyInfoWithBuildNumber -SolutionAssemblyInfoFile (Join-Path $currentDir "SolutionAssemblyInfo.cs") -Version $($buildVersion.AssemblyVersion)
-Update-NuSpecWithBuildNumber -NuSpecFile (Join-Path $currentDir "Compute.Client\Compute.Client.nuspec") -Version "$($buildVersion.PackageVersion)-$ReleaseTag"
+Update-NuSpecWithBuildNumber -NuSpecFile (Join-Path $currentDir "Compute.Client\Compute.Client.nuspec") -Version $($buildVersion.PackageVersion)
 
