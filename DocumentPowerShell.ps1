1d70f451d7bc344cd805bba82191d59b0148cb39
Anthony Shaw <anthonyshaw@apache.org>
2016-07-18 15:11:21 +1000

    add output for build txt

--- DocumentPowerShell.ps1
+++ DocumentPowerShell.ps1
@@ -1,9 +1,11 @@
+Write-Host "Importing module from build."
 Import-Module .\PowershellModule\CloudCompute.Powershell\bin\Release\DD.CBU.Compute.Powershell.dll
 $commands = Get-Command -Module CaaS
-
+Write-Host "Writing documentation"
 if((Test-Path ".\docs\powershell\") -eq 0){
     mkdir ".\docs\powershell\"}
 foreach ($command in $commands){
+    Write-Host "Writing output for $command.name"
 	.\Get-HelpToRst.ps1 $command.name | Set-Content -Encoding utf8 .\docs\powershell\$command.rst
 }
-$modPath = $Env:PSModulePath.split(';')[1]
+
