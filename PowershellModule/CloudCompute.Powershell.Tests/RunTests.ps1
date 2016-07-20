c1195cb78b991b0194e5ddfc5ab50e97c3295449
Manish Kumar <manish.kumar@itaas.dimensiondata.com>
2016-07-20 18:59:02 +1000

    First cut for PS testing , needs more refinement

---
+++ PowershellModule/CloudCompute.Powershell.Tests/RunTests.ps1
@@ -0,0 +1,4 @@
+﻿import-module (Join-Path $PSScriptRoot "..\CloudCompute.Powershell\bin\Release\CaaS.psd1")
+import-module (Join-Path $PSScriptRoot "bin\Release\CaaS_Tests.psd1")
+.\SetupConnection.ps1
+#List all the tests below
---
+++ PowershellModule/CloudCompute.Powershell.Tests/SetupConnection.ps1
@@ -0,0 +1,12 @@
+﻿# Handle the connection 
+$credential = (Get-Credential)
+$mockApiPath = (Join-Path $PSScriptRoot ".\MockApis")
+$httpClient = New-CaasApiProxyHttpClient `
+				-ApiMocksPath $mockApiPath `
+				-ApiRecordingPath $mockApiPath `
+				-ApiCredentials $credential `
+				-FallbackToDefaultApi $True `
+				-RecordApiRequestResponse $True `
+				-DefaultApiAddress 'https://api-au.dimensiondata.com/'
+
+New-CaasConnection -HttpClient $httpClient -ApiCredentials $credential
