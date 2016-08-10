3a609546ca328c284e35626ea6b57f0bcb53320d
River Yang <RiverYang@outlook.com>
2016-08-10 16:48:08 +1000

    Add DatacenterId parameter to specify the search for Get-CaasNetworkDomain

---
+++ PowershellModule/CloudCompute.Powershell.Tests/ServerTests/NetworkDomain.Tests.ps1
@@ -0,0 +1,15 @@
+﻿# Find a better way of passing the connection around
+param (		
+		[Parameter(Mandatory=$True)]
+		[DD.CBU.Compute.Powershell.Tests.TestContext] $TestContext
+		)
+
+
+Describe "Get-CaaSNetworkDomain filter by datacenterId" {
+    It "Get-CaaSNetworkDomain Should Get the list of servers" {
+		$testConnection = New-CaaSTestConnection -TestContext $TestContext
+		$domain = Get-CaasNetworkDomain -NetworkDomainName River_Lab -Connection $testConnection.CaaSConnection -DatacenterId AU9
+		$domain | Should Not BeNullOrEmpty
+		$testConnection | Verify "GET" '/caas/2.3/a4f484de-b9ed-43e4-b565-afbf69417615/network/networkDomain?name=River_Lab&datacenterId=AU9' 1
+	}
+}
