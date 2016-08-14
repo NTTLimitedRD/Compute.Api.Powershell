f5f06ad47fdd52c54e05b6a79048f8ed399d6300
River Yang <RiverYang@outlook.com>
2016-08-14 16:58:36 +1000

    Fix test case

--- PowershellModule/CloudCompute.Powershell.Tests/RunTests.ps1
+++ PowershellModule/CloudCompute.Powershell.Tests/RunTests.ps1
@@ -6,7 +6,9 @@
 		[Parameter(Mandatory=$false)]
 		[string] $BuildConfiguration = 'Debug',
 		[Parameter(Mandatory=$false)]
-		[Guid] $CaaSClientId = 'a4f484de-b9ed-43e4-b565-afbf69417615'
+		[Guid] $CaaSClientId = 'a4f484de-b9ed-43e4-b565-afbf69417615',
+		[parameter(Mandatory=$false)]
+		[PSCredential]$Cred
 		)
 
 Write-Host "!!!Using the '$BuildConfiguration' Configuration!!!"
@@ -28,7 +30,14 @@ if($UseMockCredentials -eq $True){
 }
 else{		
 	Write-Host "Using the real credentials"
-	$credential = (Get-Credential)	
+	if ($Cred -eq $null)
+	{
+		$credential = (Get-Credential)
+	}
+	else
+	{
+		$credential = $Cred
+	}
 }
 
 $testContext = New-CaasTestContext -UseMockCredentials $UseMockCredentials -FallbackToDefaultApi $FallbackToDefaultApi -MockApisPath $mockApiPath -MockApisRecordingPath $mockApiRecordingPath -ApiCredentials $credential -DefaultApiAddress 'https://api-au.dimensiondata.com/' -RecordApiRequestResponse $True -CaaSClientId $CaaSClientId
