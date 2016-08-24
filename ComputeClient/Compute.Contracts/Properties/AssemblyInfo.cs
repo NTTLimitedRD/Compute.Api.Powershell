238db32921ede4028069a11428dc8452f50cfecb
Manish Kumar <manish.kumar@itaas.dimensiondata.com>
2016-08-24 13:14:31 +1000

    Removing the Compute Client code from this repo as it sits in a different one now

--- ComputeClient/Compute.Contracts/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("DD.CBU.Compute.Api.Contracts")]
-[assembly: AssemblyDescription("Data-contract library for Dimension Data CaaS API")]
-
-[assembly: ComVisible(false)]
-[assembly: Guid("6e8c8500-f233-499a-b088-2ff3e6d832b9")]
