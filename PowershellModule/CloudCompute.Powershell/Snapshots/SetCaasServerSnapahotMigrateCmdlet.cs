bb7a880e96c648ab6c68aacd8b0e01f5deaa30cb
Sanu Sathyaseelan <sanu.sathyaseelan@itaas.dimensiondata.com>
2018-02-14 09:40:58 +1100

    CCI-489 Added Snapshot Preview Server Migrate cmdlet

---
+++ PowershellModule/CloudCompute.Powershell/Snapshots/SetCaasServerSnapahotMigrateCmdlet.cs
@@ -0,0 +1,48 @@
+﻿namespace DD.CBU.Compute.Powershell.Mcp20
+{
+    using System;
+    using System.Management.Automation;
+    using Api.Client;
+    using Api.Contracts.Network20;
+
+    /// <summary>
+    ///     The set server snapshot migrate cmdlet.
+    /// </summary>
+    [Cmdlet("Set", "CaasServerSnapshotMigrate")]
+    [OutputType(typeof(ResponseType))]
+    public class SetCaasServerSnapahotMigrateCmdlet : PsCmdletCaasServerBase
+    {
+        /// <summary>
+        ///     The process record method.
+        /// </summary>
+        protected override void ProcessRecord()
+        {
+            ResponseType response = null;
+            base.ProcessRecord();
+            try
+            {
+                response = Connection.ApiClient.Snapshot.MigrateSnapshotPreviewServer(new ServerIdType { serverId = Server.id }).Result;
+            }
+            catch (AggregateException ae)
+            {
+                ae.Handle(
+                    e =>
+                    {
+                        if (e is ComputeApiException)
+                        {
+                            WriteError(new ErrorRecord(e, "-2", ErrorCategory.InvalidOperation, Connection));
+                        }
+                        else
+                        {
+                            // if (e is HttpRequestException)
+                            ThrowTerminatingError(new ErrorRecord(e, "-1", ErrorCategory.ConnectionError, Connection));
+                        }
+
+                        return true;
+                    });
+            }
+
+            WriteObject(response);
+        }
+    }
+}
