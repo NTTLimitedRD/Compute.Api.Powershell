6aa6f21dbf98abb64c568de17593b48ad55d8409
Sanu Sathyaseelan <sanu.sathyaseelan@itaas.dimensiondata.com>
2018-02-13 14:27:49 +1100

    Added Enable/Disable Snapshot For server and Get Snapshot Windows In a daracenter

---
+++ PowershellModule/CloudCompute.Powershell/Snapshots/EnableCaasServerSnapshotCmdlet.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Management.Automation;
+using DD.CBU.Compute.Api.Client;
+using DD.CBU.Compute.Api.Contracts.Network20;
+
+namespace DD.CBU.Compute.Powershell.Mcp20
+{
+    /// <summary>
+    ///     The Enable Caas Server Snapshot CmdLet
+    /// </summary>
+    [Cmdlet(VerbsLifecycle.Enable, "CaasServerSnapshot")]
+    [OutputType(typeof(ResponseType))]
+    public class EnableCaasServerSnapshotCmdlet : PSCmdletCaasWithConnectionBase
+    {
+        /// <summary>
+        ///     Gets or sets Server.
+        /// </summary>
+        [Parameter(Mandatory = true, ValueFromPipeline = true, HelpMessage = "The Server")]
+        public ServerType Server { get; set; }
+
+        /// <summary>
+        /// Gets or sets Service Plan
+        /// </summary>
+        [Parameter(Mandatory = true, HelpMessage = "Snapshot service plan")]
+        public string ServicePlan { get; set; }
+
+        /// <summary>
+        /// Gets or sets Snapshot Window
+        /// </summary>
+        [Parameter(Mandatory = true, HelpMessage = "Snapshot Window Id")]
+        public Guid SnapshotWindowId { get; set; }
+
+        /// <summary>
+        ///     The process record method.
+        /// </summary>
+        protected override void ProcessRecord()
+        {
+            ResponseType response = null;
+            base.ProcessRecord();
+            try
+            {
+                var snapshotType = new EnableSnapshotServiceType
+                {
+                    serverId = Server.id,
+                    servicePlan = ServicePlan,
+                    windowId = SnapshotWindowId.ToString(),
+                    initiateManualSnapshot = false
+                };
+
+                response = Connection.ApiClient.ServerManagement.Server.EnableSnapshotService(snapshotType).Result;
+            }
+            catch (AggregateException ae)
+            {
+                ae.Handle(
+                    e =>
+                    {
+                        if (e is ComputeApiException)
+                        {
+                            WriteError(new ErrorRecord(e, "-2", ErrorCategory.InvalidOperation, Connection));
+                        }
+                        else
+                        {
+                            // if (e is HttpRequestException)
+                            ThrowTerminatingError(new ErrorRecord(e, "-1", ErrorCategory.ConnectionError, Connection));
+                        }
+
+                        return true;
+                    });
+            }
+
+            WriteObject(response);
+        }
+    }
+}
---
+++ PowershellModule/CloudCompute.Powershell/Snapshots/RemoveCaasServerSnapshotCmdlet.cs
@@ -0,0 +1,50 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="RemoveCaasServerDiskCmdlet.cs" company="">
+//   
+// </copyright>
+// <summary>
+//   The set server state cmdlet.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System;
+using System.Management.Automation;
+using DD.CBU.Compute.Api.Client;
+
+namespace DD.CBU.Compute.Powershell
+{
+    [Cmdlet(VerbsCommon.Remove, "CaasServerSnapshot", SupportsShouldProcess = true)]
+    public class RemoveCaasServerSnapshotCmdlet : PsCmdletCaasServerBase
+    {
+        /// <summary>
+        ///     The process record method.
+        /// </summary>
+        protected override void ProcessRecord()
+        {
+            base.ProcessRecord();
+            try
+            {
+                WriteObject(Connection.ApiClient.ServerManagement.Server.DisableSnapshotService(new Api.Contracts.Network20.ServerIdType { serverId = Server.id }).Result);
+            }
+            catch (AggregateException ae)
+            {
+                ae.Handle(
+                    e =>
+                    {
+                        if (e is ComputeApiException)
+                        {
+                            WriteError(new ErrorRecord(e, "-2", ErrorCategory.InvalidOperation, Connection));
+                        }
+                        else
+                        {
+                            // if (e is HttpRequestException)
+                            ThrowTerminatingError(
+                                new ErrorRecord(e, "-1", ErrorCategory.ConnectionError, Connection));
+                        }
+
+                        return true;
+                    });
+            }
+        }
+    }
+}
